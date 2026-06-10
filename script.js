1aeb31d7189546a86f248b742d841c377bb7f4f8
iqbolshoh <iilhomjonov777@gmail.com>
2026-06-10 17:17:48 +0500

    toliq ozgarish

---
+++ script.js
@@ -0,0 +1,22 @@
+async function getWeather() {
+  const city = document.getElementById("city").value;
+  const result = document.getElementById("result");
+
+  const apiKey = "YOUR_API_KEY";
+
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+  );
+
+  const data = await res.json();
+
+  if (data.cod === 200) {
+    result.innerHTML = `
+      <h2>📍 ${data.name}</h2>
+      <h1>🌡 ${data.main.temp}°C</h1>
+      <p>🌥 ${data.weather[0].description}</p>
+    `;
+  } else {
+    result.innerHTML = "❌ City not found";
+  }
+}
